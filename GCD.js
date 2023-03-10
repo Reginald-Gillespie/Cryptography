253fd6ae96eb249f6dda15943d7fb7067531fcc3
WKoA <109245101+Reginald-Gillespie@users.noreply.github.com>
2023-03-10 08:16:41 -0600

    Update GCD.js

--- GCD.js
+++ GCD.js
@@ -6,3 +6,21 @@ function gcd(a, b) {
     }
     return a;
 }
+
+// What it says
+function extendedEuclidean(p, q) {
+  let u1 = 1, v1 = 0, u2 = 0, v2 = 1;
+  while (q !== 0) {
+    let r = p % q;
+    let q_ = p / q >> 0;
+    let u3 = u1 - q_ * u2;
+    let v3 = v1 - q_ * v2;
+    p = q;
+    q = r;
+    u1 = u2;
+    v1 = v2;
+    u2 = u3;
+    v2 = v3;
+  }
+  return { u: u1, v: v1 };
+}
