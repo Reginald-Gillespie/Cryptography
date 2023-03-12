b3ae4301a6717e275b11288c7b8f4288b237de07
WKoA <109245101+Reginald-Gillespie@users.noreply.github.com>
2023-03-12 12:26:48 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,9 +1,12 @@
 function sqrt() {
   var p = 29;
-  var find = 16;
-  for (var i = 0; i < p; i++) {
+  var find = 6;
+  var residue = (Math.pow(find, (p-1) >> 1) % p)==1;
+  console.log("is residue: " + residue)
+  if (residue) for (var i = 0; i < p; i++) {
       if (i**2 % p === find) {
           console.log(i);
       }
   }
 }
+
